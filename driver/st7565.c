f544a75639e14d816245bfe4c81624aed85eebf9
Pixel <me.pixelgirl@gmail.com>
2024-03-15 11:37:23 +0000

    Updated code formatting

--- driver/st7565.c
+++ driver/st7565.c
@@ -19,7 +19,7 @@
 
 #include <stdint.h>
 #include <stdio.h>     // NULL
-
+#include <string.h>
 #include "bsp/dp32g030/gpio.h"
 #include "bsp/dp32g030/spi.h"
 #include "driver/gpio.h"
@@ -31,8 +31,7 @@
 uint8_t gStatusLine[128];
 uint8_t gFrameBuffer[7][128];
 
-void ST7565_DrawLine(const unsigned int Column, const unsigned int Line, const unsigned int Size, const uint8_t *pBitmap)
-{
+void ST7565_DrawLine(const unsigned int Column, const unsigned int Line, const unsigned int Size, const uint8_t *pBitmap){
 	unsigned int i;
 
 	SPI_ToggleMasterMode(&SPI0->CR, false);
@@ -41,18 +40,13 @@ void ST7565_DrawLine(const unsigned int Column, const unsigned int Line, const u
 
 	GPIO_SetBit(&GPIOB->DATA, GPIOB_PIN_ST7565_A0);
 
-	if (pBitmap != NULL)
-	{
-		for (i = 0; i < Size; i++)
-		{
+	if (pBitmap != NULL){
+		for (i = 0; i < Size; i++){
 			while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 			SPI0->WDR = pBitmap[i];
 		}
-	}
-	else
-	{
-		for (i = 0; i < Size; i++)
-		{
+	}else{
+		for (i = 0; i < Size; i++){
 			while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 			SPI0->WDR = 0;
 		}
@@ -63,21 +57,18 @@ void ST7565_DrawLine(const unsigned int Column, const unsigned int Line, const u
 	SPI_ToggleMasterMode(&SPI0->CR, true);
 }
 
-void ST7565_BlitFullScreen(void)
-{
+void ST7565_BlitFullScreen(void){
 	unsigned int Line;
 
 	SPI_ToggleMasterMode(&SPI0->CR, false);
 
 	ST7565_WriteByte(0x40);
 
-	for (Line = 0; Line < ARRAY_SIZE(gFrameBuffer); Line++)
-	{
+	for (Line = 0; Line < ARRAY_SIZE(gFrameBuffer); Line++){
 		unsigned int Column;
 		ST7565_SelectColumnAndLine(4, Line + 1);
 		GPIO_SetBit(&GPIOB->DATA, GPIOB_PIN_ST7565_A0);
-		for (Column = 0; Column < ARRAY_SIZE(gFrameBuffer[0]); Column++)
-		{
+		for (Column = 0; Column < ARRAY_SIZE(gFrameBuffer[0]); Column++){
 			while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 			SPI0->WDR = gFrameBuffer[Line][Column];
 		}
@@ -94,9 +85,7 @@ void ST7565_BlitFullScreen(void)
 	SPI_ToggleMasterMode(&SPI0->CR, true);
 }
 
-void ST7565_BlitStatusLine(void)
-{	// the top small text line on the display
-
+void ST7565_BlitStatusLine(void){	// the top small text line on the display
 	unsigned int i;
 
 	SPI_ToggleMasterMode(&SPI0->CR, false);
@@ -107,8 +96,7 @@ void ST7565_BlitStatusLine(void)
 
 	GPIO_SetBit(&GPIOB->DATA, GPIOB_PIN_ST7565_A0);
 
-	for (i = 0; i < ARRAY_SIZE(gStatusLine); i++)
-	{
+	for (i = 0; i < ARRAY_SIZE(gStatusLine); i++){
 		while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 		SPI0->WDR = gStatusLine[i];
 	}
@@ -118,8 +106,7 @@ void ST7565_BlitStatusLine(void)
 	SPI_ToggleMasterMode(&SPI0->CR, true);
 }
 
-void ST7565_FillScreen(uint8_t Value)
-{
+void ST7565_FillScreen(uint8_t Value){
 	unsigned int i;
 
 	// reset some of the displays settings to try and overcome the radios hardware problem - RF corrupting the display
@@ -127,13 +114,11 @@ void ST7565_FillScreen(uint8_t Value)
 	
 	SPI_ToggleMasterMode(&SPI0->CR, false);
 
-	for (i = 0; i < 8; i++)
-	{
+	for (i = 0; i < 8; i++){
 		unsigned int j;
 		ST7565_SelectColumnAndLine(0, i);
 		GPIO_SetBit(&GPIOB->DATA, GPIOB_PIN_ST7565_A0);
-		for (j = 0; j < 132; j++)
-		{
+		for (j = 0; j < 132; j++){
 			while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 			SPI0->WDR = Value;
 		}
@@ -208,20 +193,16 @@ uint8_t cmds[] = {
 	ST7565_CMD_DISPLAY_ON_OFF | 1,			// Display ON/OFF: ON
 };
 
-void ST7565_Init(const bool full)
-{
+void ST7565_Init(const bool full){
 	if (full) {
 		SPI0_Init();
 		ST7565_HardwareReset();
 		SPI_ToggleMasterMode(&SPI0->CR, false);
 		ST7565_WriteByte(ST7565_CMD_SOFTWARE_RESET);   // software reset
 		SYSTEM_DelayMs(120);
-	}
-	else
-		SPI_ToggleMasterMode(&SPI0->CR, false);
+	}else SPI_ToggleMasterMode(&SPI0->CR, false);
 
-	for(uint8_t i = 0; i < 8; i++)
-		ST7565_WriteByte(cmds[i]);
+	for(uint8_t i = 0; i < 8; i++) ST7565_WriteByte(cmds[i]);
 
 	if (full) {
 		ST7565_WriteByte(ST7565_CMD_POWER_CIRCUIT | 0b011);   // VB=0 VR=1 VF=1
@@ -240,12 +221,10 @@ void ST7565_Init(const bool full)
 	SPI_WaitForUndocumentedTxFifoStatusBit();
 	SPI_ToggleMasterMode(&SPI0->CR, true);
 
-	if (full)
-		ST7565_FillScreen(0x00);
+	if (full) ST7565_FillScreen(0x00);
 }
 
-void ST7565_FixInterfGlitch(void)
-{
+void ST7565_FixInterfGlitch(void){
 	SPI_ToggleMasterMode(&SPI0->CR, false);
 	for(uint8_t i = 0; i < ARRAY_SIZE(cmds); i++)
 		ST7565_WriteByte(cmds[i]);
@@ -253,8 +232,7 @@ void ST7565_FixInterfGlitch(void)
 	SPI_ToggleMasterMode(&SPI0->CR, true);
 }
 
-void ST7565_HardwareReset(void)
-{
+void ST7565_HardwareReset(void){
 	GPIO_SetBit(&GPIOB->DATA, GPIOB_PIN_ST7565_RES);
 	SYSTEM_DelayMs(1);
 	GPIO_ClearBit(&GPIOB->DATA, GPIOB_PIN_ST7565_RES);
@@ -263,8 +241,7 @@ void ST7565_HardwareReset(void)
 	SYSTEM_DelayMs(120);
 }
 
-void ST7565_SelectColumnAndLine(uint8_t Column, uint8_t Line)
-{
+void ST7565_SelectColumnAndLine(uint8_t Column, uint8_t Line){
 	GPIO_ClearBit(&GPIOB->DATA, GPIOB_PIN_ST7565_A0);
 	while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 	SPI0->WDR = Line + 176;
@@ -275,8 +252,7 @@ void ST7565_SelectColumnAndLine(uint8_t Column, uint8_t Line)
 	SPI_WaitForUndocumentedTxFifoStatusBit();
 }
 
-void ST7565_WriteByte(uint8_t Value)
-{
+void ST7565_WriteByte(uint8_t Value){
 	GPIO_ClearBit(&GPIOB->DATA, GPIOB_PIN_ST7565_A0);
 	while ((SPI0->FIFOST & SPI_FIFOST_TFF_MASK) != SPI_FIFOST_TFF_BITS_NOT_FULL) {}
 	SPI0->WDR = Value;
